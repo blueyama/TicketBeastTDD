966c1a3b4e523371b31f089a4ddccb46e701e97f
Desmond Rush <blueyama@gmail.com>
2018-12-06 17:18:19 -0700

    Video step completed "Refactoring for Speed"  - learned about setting model attributes on model magically

--- app/Concert.php
+++ app/Concert.php
@@ -10,8 +10,30 @@ class Concert extends Model
     protected $guarded = [];
     protected $dates = ['date'];
 
+    /**
+     * this is accessible in the blade view like so Concert::formatted_date
+     * @return mixed
+     */
     public function getFormattedDateAttribute()
     {
         return $this->date->format('F j, Y');
+    }
+
+    /**
+     * this is accessible in the blade view like so Concert::formatted_start_time
+     * @return mixed
+     */
+    public function getFormattedStartTimeAttribute()
+    {
+        return $this->date->format('g:ia');
+    }
+
+    /**
+     * this is accessible in the blade view like so Concert::ticket_price_in_dollars
+     * @return mixed
+     */
+    public function getTicketPriceInDollarsAttribute()
+    {
+        return number_format($this->ticket_price / 100, 2);
     }
 }
